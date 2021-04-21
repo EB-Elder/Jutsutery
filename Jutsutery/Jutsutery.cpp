acc861f0e6573b47b5d481201f8a386ba30cb67a
Régis <regis.poizat@cegetel.net>
2021-04-21 16:56:17 +0200

    huphuphup balbatruc

--- Jutsutery/Jutsutery.cpp
+++ Jutsutery/Jutsutery.cpp
@@ -5,10 +5,14 @@
 #include <opencv2/video.hpp>
 #include <opencv2/core.hpp>
 #include <iostream>
+#include <vector>
+#include <chrono>
 
 using namespace std;
 using namespace cv;
 using namespace cv::dnn;
+using std::chrono::nanoseconds;
+using std::chrono::duration_cast;
 
 const int POSE_PAIRS[20][2] =
 {
@@ -31,8 +35,6 @@ Mat nextInput, frameCopy;
 vector<uchar> status;
 vector<float> err;
 
-
-
 void detectPoints(Mat& img)
 {
     cv::goodFeaturesToTrack(img, prevPoints, 500, 0.01, 1);
@@ -53,7 +55,7 @@ void trackPoints()
         if (!(prevPoints.size() > 0))
         {
             detectPoints(prevInput);
-            cout << nextPoints.size();
+            std::cout << nextPoints.size();
             if (!(prevPoints.size() > 0))
                 return;
         }
@@ -62,13 +64,14 @@ void trackPoints()
         purgePoints(nextPoints, status);
         purgePoints(prevPoints, status);
     }
-    
 }
 
 int main(int argc, char** argv)
 {
+	//seuil ??
     float thresh = 0.01;
 
+	//flux vidéo
     cv::VideoCapture cap(1);
 
     if (!cap.isOpened())
@@ -76,32 +79,61 @@ int main(int argc, char** argv)
         cerr << "Unable to connect to camera" << endl;
         return 1;
     }
-
-    
+   
+	//dimensions de la frame
     int frameWidth = cap.get(CAP_PROP_FRAME_WIDTH);
     int frameHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
     float aspect_ratio = frameWidth / (float)frameHeight;
-    int inHeight = 64;
+
+	//resize de l'image à traiter
+    int inHeight = 300;
     int inWidth = (int(aspect_ratio * inHeight) * 8) / 8;
 
-    cout << "inWidth = " << inWidth << " ; inHeight = " << inHeight << endl;
+    std::cout << "inWidth = " << inWidth << " ; inHeight = " << inHeight << endl;
 
+	//écriture du résultat dans un fichier vidéo
     VideoWriter video("Output-Skeleton.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, Size(frameWidth, frameHeight));
 
     Net net = readNetFromCaffe(protoFile, weightsFile);
+	net.setPreferableTarget(cv::dnn::DNN_TARGET_OPENCL);
+	net.setPreferableBackend(cv::dnn::DNN_BACKEND_DEFAULT);
 
+	//temps de traitement
     double t = 0;
+
+	//timer
+	typedef std::chrono::high_resolution_clock Clock;
+	auto start = Clock::now();
+	std::chrono::time_point<std::chrono::steady_clock>last_forward;
+
+	bool first = true;
+
+	Mat output;
+
+	//boucle de traitement
     while (1)
     {
-        double t = (double)cv::getTickCount();
+		//timer
+		auto time_newframe = Clock::now();
+
+		nanoseconds ti = duration_cast<nanoseconds>(time_newframe - start);
+		nanoseconds ns = duration_cast<nanoseconds>(time_newframe - last_forward);
 
         cap >> nextInput;
         frameCopy = nextInput.clone();
-        Mat inpBlob = blobFromImage(nextInput, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
 
-        /*net.setInput(inpBlob);
+		if (first || ns.count() > 500000000)
+		{
+			Mat inpBlob = blobFromImage(nextInput, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
+			net.setInput(inpBlob);
 
-        Mat output = net.forward();
+			output = net.forward();
+			first = false;
+
+			last_forward = Clock::now();
+		}      
+
+	    t = (double)cv::getTickCount();
 
         int H = output.size[2];
         int W = output.size[3];
@@ -128,7 +160,37 @@ int main(int argc, char** argv)
 
         int nPairs = sizeof(POSE_PAIRS) / sizeof(POSE_PAIRS[0]);
 
-        for (int n = 0; n < nPairs; n++)
+		Point2d barycentre(0.0f, 0.0f);
+		float minX = frameWidth;
+		float minY = frameHeight;
+		float maxX = 0.0f;
+		float maxY = 0.0f;
+
+		for (int n = 0; n < nPoints; n++)
+		{
+			if (points[n].x < minX) minX = points[n].x;
+			else
+			{
+				if (points[n].x > maxX) maxX = points[n].x;
+			}
+
+			if (points[n].y < minY) minY = points[n].y;
+			else
+			{
+				if (points[n].y > maxY) maxY = points[n].y;
+			}
+
+			barycentre.x = barycentre.x + points[n].x;
+			barycentre.y = barycentre.y + points[n].y;
+		}
+
+		barycentre.x = barycentre.x / (float)nPoints;
+		barycentre.y = barycentre.y / (float)nPoints;
+
+		circle(nextInput, barycentre, 8, Scalar(0, 0, 255), -1);
+		rectangle(nextInput, Point2d(minX, minY), Point2d(maxX, maxY), Scalar(0, 0, 255), 8);
+
+        /*for (int n = 0; n < nPairs; n++)
         {
             // lookup 2 connected body/hand parts
             Point2f partA = points[POSE_PAIRS[n][0]];
@@ -137,17 +199,18 @@ int main(int argc, char** argv)
             if (partA.x <= 0 || partA.y <= 0 || partB.x <= 0 || partB.y <= 0)
                 continue;
 
-            line(frame, partA, partB, Scalar(0, 255, 255), 8);
-            circle(frame, partA, 8, Scalar(0, 0, 255), -1);
-            circle(frame, partB, 8, Scalar(0, 0, 255), -1);
+            line(nextInput, partA, partB, Scalar(0, 255, 255), 8);
+            circle(nextInput, partA, 8, Scalar(0, 0, 255), -1);
+            circle(nextInput, partB, 8, Scalar(0, 0, 255), -1);
         }*/
-
-        cvtColor(nextInput, nextInput, COLOR_BGR2GRAY);
+      
+		cv::cvtColor(nextInput, nextInput, COLOR_BGR2GRAY);
         trackPoints();
         prevInput = nextInput.clone();
-        cvtColor(nextInput, nextInput, COLOR_GRAY2BGR);
-        cout << nextPoints.size();
-        for (size_t i = 0; i < prevPoints.size(); i++)
+        cv::cvtColor(nextInput, nextInput, COLOR_GRAY2BGR);
+        std::cout << nextPoints.size();
+
+        for (std::size_t i = 0; i < prevPoints.size(); i++)
         {
             circle(nextInput, prevPoints[i], 10, Scalar(0, 0, 255));
             line(nextInput, prevPoints[i], nextPoints[i], Scalar(0, 255, 0));
@@ -155,10 +218,10 @@ int main(int argc, char** argv)
         }
 
         t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-        cout << "Time Taken for frame = " << t << endl;
+        //std::cout << "Time Taken for frame = " << t << endl;
         cv::putText(nextInput, cv::format("time taken = %.2f sec", t), cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, .8, cv::Scalar(255, 50, 0), 2);
         // imshow("Output-Keypoints", frameCopy);
-        imshow("Output-Skeleton", nextInput);
+        cv::imshow("Output-Skeleton", nextInput);
         video.write(nextInput);
         char key = waitKey(1);
         if (key == 27)
